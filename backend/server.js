42df4c27bd247fcc4951aedf95814cf4c6f76397
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-02 23:18:42 +0530

    MongoDb Connection is established

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,25 @@ const Db_name = process.env.DB_NAME || "DEV_DB";
 const MongoUri =
   process.env.MONGO_URI || `mongodb://localhost:27017/${Db_name}`;
 const port = process.env.PORT || 3333;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Db connection
+
+mongoose.connect(MongoUri);
+
+db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log(error);
+  console.log("Error connecting server");
+});
+db.once("open", () => {
+  console.log("[*] connected to Db");
+
+  app.listen(port, () => {
+    console.log(`[+] API getway is ${port}`);
+  });
+});
