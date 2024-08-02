de31108bca51d5a3012e27dfd74dc84c4a3d1bad
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-02 23:17:39 +0530

    Importing Dep & Setting Vars

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
 console.log("[+] Initializing the Server ");
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const Db_name = process.env.DB_NAME || "DEV_DB";
+const MongoUri =
+  process.env.MONGO_URI || `mongodb://localhost:27017/${Db_name}`;
+const port = process.env.PORT || 3333;
