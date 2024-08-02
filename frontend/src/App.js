f769a301b9e25626c42cb5c20d58ebca08fcf0ed
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-02 23:43:34 +0530

    Login Page and CSS is Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/Pages/LoginPage';
 
 function App() {
   return (
     <div className="App">
-
+      <LoginPage />
     </div>
   );
 }
