e3519ed8bc3227ae1a511ec0abecbd26726d0a66
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-02 23:21:31 +0530

    User Account Model is Created

---
+++ backend/Models/UserModelSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  account: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("Users", UserSchema);
