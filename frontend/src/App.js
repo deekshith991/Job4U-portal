89a84509b79d3a4aba95e5c96577e37364266123
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-03 09:58:41 +0530

    Routing is settup on App.js at Not logged In case

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import { useAuth } from './components/services.js/AuthContext';
 import LoginPage from './components/Pages/LoginPage';
 import RegisterPage from './components/Pages/RegisterPage';
 
 function App() {
+
+  const { authData } = useAuth();
+
   return (
     <div className="App">
-      <LoginPage />
-      <RegisterPage />
+      <Router>
+
+        {authData.isLoggedIn ?
+          //Logged In case Yes
+          <h1>logged In</h1>
+          :
+
+          // Not Logged In case No
+          <Routes>
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/' element={<LoginPage />} />
+          </Routes>
+        }
+
+      </Router>
     </div>
   );
 }
