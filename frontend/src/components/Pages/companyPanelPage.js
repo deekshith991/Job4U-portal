24026d307fa518f763ef8a841aa4845f34cfd571
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-10 22:24:54 +0530

    basic IssueJob component created and added to Company panel

---
+++ frontend/src/components/Pages/companyPanelPage.js
@@ -0,0 +1,14 @@
+import IssueJob from "../Sections/IssueJob"
+
+
+
+const CompanyPage = () => {
+
+    return (
+        <div className="basepage">
+            <IssueJob />
+        </div>
+    )
+}
+
+export default CompanyPage;
