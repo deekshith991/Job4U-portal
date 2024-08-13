90a7609aa7623de514eb6a9db8d052827f9add63
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-13 22:57:24 +0530

    job gallery styling done

--- frontend/src/components/Sections/JobCard.js
+++ frontend/src/components/Sections/JobCard.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // Optional, but good for type checking
 
 const JobCard = ({ companyName, position, salary, lastDate }) => {
     return (
@@ -13,24 +12,12 @@ const JobCard = ({ companyName, position, salary, lastDate }) => {
                 <h4>Position: {position}</h4>
                 <h4>Salary: ${salary}</h4> {/* Added currency symbol for better clarity */}
                 <h4>Last Date: {lastDate}</h4>
+                <button disabled className="ApplyBtn">view more</button>
                 <button disabled className="ApplyBtn">Apply</button>
+
             </div>
         </div>
     );
-};
-
-// Optional: Add PropTypes for validation
-JobCard.propTypes = {
-    companyName: PropTypes.string.isRequired,
-    position: PropTypes.string.isRequired,
-    salary: PropTypes.number,
-    lastDate: PropTypes.string
-};
-
-// Optional: Set default props
-JobCard.defaultProps = {
-    salary: 0, // Default salary if not provided
-    lastDate: 'N/A' // Default last date if not provided
 };
 
 export default JobCard;
