78596c0fb912d79636a73cb2a9bea1f0a9feb4fe
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-31 23:18:04 +0530

    Update: footer added

---
+++ frontend/src/component/Sections/Footer.js
@@ -0,0 +1,15 @@
+import '../css/Header.css';
+
+
+const Footer = () => {
+
+    return (
+        <div className="Footer">
+            <footer>
+                <p>Copy righted</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
