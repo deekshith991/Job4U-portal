68b45aee98301ab0d092a816acc351011ed34ac8
vaggu Deekshith <vaggudeekshith@oulook.com>
2024-08-31 22:27:59 +0530

    Update: pages added

---
+++ frontend/src/component/Pages/HomePage.js
@@ -0,0 +1,21 @@
+import Header from "../Sections/Header"
+
+
+const HomePage = () => {
+
+    return (
+
+        <div className="HomePage">
+            <div>
+                <Header />
+            </div>
+
+
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
---
+++ frontend/src/component/Pages/LoginPage.js
@@ -0,0 +1,13 @@
+import LoginCard from "../Sections/LoginCard";
+
+
+const LoginPage = () => {
+
+    return (
+        <div className="loginpage">
+            <LoginCard />
+        </div>
+    )
+}
+
+export default LoginPage;
